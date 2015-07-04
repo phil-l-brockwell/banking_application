8ea662d812a6ec277eb2287c4394bdb6c281dce2
Phil Brockwell <nineteen40eight@gmail.com>
2015-07-04 17:14:43 +0100

    added some helper methods to spec

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -11,6 +11,10 @@ describe 'Controller' do
     test_controller.create_holder('Robert Pulson')
   end
 
+  def open_account(type = :savings, holder = test_holder)
+    test_controller.open_account(type, holder)
+  end
+
   context 'when initialised' do
     it 'has a hash of accounts' do
       expect(test_controller).to respond_to(:accounts)
@@ -31,115 +35,110 @@ describe 'Controller' do
 
   context 'when creating a holder' do
     it 'increments the holder number' do
-      expect { test_controller.create_holder('Robert Pulson') }
-        .to change { test_controller.holder_id }.by(1)
+      expect { create_holder }.to change { test_controller.holder_id }.by(1)
     end
 
     it 'returns the new holders id' do
-      num = test_controller.holder_id
-      expect(test_controller.create_holder('Robert Pulson'))
-        .to eq(num)
+      new_id = test_controller.holder_id
+      expect(create_holder).to eq(new_id)
     end
 
     it 'gives the new holder the correct holder id' do
-      num = test_controller.create_holder('Robert Pulson')
-      expect(test_controller.holders[num].id).to eq(num)
+      new_id = create_holder
+      expect(test_controller.holders[new_id].id).to eq(new_id)
     end
 
     it 'adds the new holder to the holders hash' do
-      num = test_controller.create_holder('Robert Pulson')
-      expect(test_controller.holders[num].name).to eq('Robert Pulson')
+      new_id = create_holder
+      expect(test_controller.holders[new_id].name).to eq('Robert Pulson')
     end
   end
 
   context 'when opening an account' do
     it 'returns the new accounts id' do
-      holder_num = create_holder
-      num = test_controller.account_id
-      expect(test_controller.open_account(:savings, test_holder))
-        .to eq(num)
+      new_id = test_controller.account_id
+      expect(open_account).to eq(new_id)
     end
 
     it 'increments the account id' do
-      expect { test_controller.open_account(:current, test_holder) }
-        .to change { test_controller.account_id }.by(1)
+      expect { open_account }.to change { test_controller.account_id }.by(1)
     end
 
     it 'gives the new account the correct account id' do
-      num = test_controller.open_account(:current, test_holder)
-      expect(test_controller.accounts[num].id).to eq(num)
+      new_id = open_account
+      expect(test_controller.accounts[new_id].id).to eq(new_id)
     end
 
     it 'adds the new account to the accounts hash' do
-      num = test_controller.open_account(:current, test_holder)
-      expect(test_controller.accounts[num].main_holder).to eq(test_holder)
-      expect(test_controller.accounts[num]).to respond_to(:balance)
-      expect(test_controller.accounts[num].type).to eq(:current)
+      new_id = open_account
+      expect(test_controller.accounts[new_id].main_holder).to eq(test_holder)
+      expect(test_controller.accounts[new_id]).to respond_to(:balance)
+      expect(test_controller.accounts[new_id].type).to eq(:savings)
     end
 
-    it 'can open a savings account' do
-      num = test_controller.open_account(:savings, test_holder)
-      expect(test_controller.accounts[num].type).to eq(:savings)
+    it 'can open different account types' do
+      new_id = open_account(:current)
+      expect(test_controller.accounts[new_id].type).to eq(:current)
     end
   end
 
   context 'when transacting' do
     it 'can make a deposit' do
-      num = test_controller.open_account(:savings, test_holder)
-      expect(test_controller.accounts[num]).to receive(:deposit).with(50.00)
-      test_controller.deposit_into(num, 50.00)
+      new_id = open_account
+      expect(test_controller.accounts[new_id]).to receive(:deposit).with(50.00)
+      test_controller.deposit_into(new_id, 50.00)
     end
 
     it 'can make a withdrawal' do
-      num = test_controller.open_account(:savings, test_holder)
-      expect(test_controller.accounts[num]).to receive(:withdraw).with(50.00)
-      test_controller.withdraw_from(num, 50.00)
+      new_id = open_account
+      expect(test_controller.accounts[new_id]).to receive(:withdraw).with(50.00)
+      test_controller.withdraw_from(new_id, 50.00)
     end
 
     it 'can make a transfer between two accounts' do
-      num_1 = test_controller.open_account(:savings, test_holder)
-      test_controller.deposit_into(num_1, 10.00)
-      num_2 = test_controller.open_account(:savings, test_holder)
-      expect(test_controller.accounts[num_1]).to receive(:withdraw).with(10.00)
-      expect(test_controller.accounts[num_2]).to receive(:deposit).with(10.00)
-      test_controller.transfer_between(num_1, num_2, 10.00)
+      new_id = open_account
+      test_controller.deposit_into(new_id, 10.00)
+      new_id_2 = open_account
+      expect(test_controller.accounts[new_id]).to receive(:withdraw).with(10.00)
+      expect(test_controller.accounts[new_id_2]).to receive(:deposit).with(10.00)
+      test_controller.transfer_between(new_id, new_id_2, 10.00)
     end
 
     it 'raises an error if the donar account has insufficient funds' do
-      num_1 = test_controller.open_account(:savings, test_holder)
-      num_2 = test_controller.open_account(:savings, test_holder)
-      expect { test_controller.transfer_between(num_1, num_2, 10.00) }
+      new_id = open_account
+      new_id_2 = open_account
+      expect { test_controller.transfer_between(new_id, new_id_2, 10.00) }
         .to raise_error('The withdrawal amount exceeds current balance!')
     end
 
     it 'can pay the interest on an account' do
-      num_1 = test_controller.open_account(:savings, test_holder)
-      test_controller.deposit_into(num_1, 10.00)
-      expect { test_controller.pay_interest_on(num_1) }
-        .to change { test_controller.accounts[num_1].balance }
+      new_id = open_account
+      test_controller.deposit_into(new_id, 10.00)
+      expect { test_controller.pay_interest_on(new_id) }
+        .to change { test_controller.accounts[new_id].balance }
     end
 
     it 'can add a holder to an account' do
-      num_1 = test_controller.open_account(:savings, test_holder)
-      expect(test_controller.accounts[num_1]).to receive(:add_holder)
+      new_id = open_account
+      expect(test_controller.accounts[new_id]).to receive(:add_holder)
         .with(second_holder)
-      test_controller.add_holder_to(num_1, second_holder)
+      test_controller.add_holder_to(new_id, second_holder)
     end
   end
 
   it 'can return all transactions of a given account' do
-    num = test_controller.open_account(:savings, test_holder)
+    new_id = open_account
     new_transaction = double :new_transaction, type: :deposit, amount: 50.00
-    test_controller.accounts[num].add_transaction(new_transaction)
-    expect(test_controller.get_transactions_of(num))
+    test_controller.accounts[new_id].add_transaction(new_transaction)
+    expect(test_controller.get_transactions_of(new_id))
       .to eq([new_transaction])
   end
 
   it 'can return all accounts for a given holder' do
-    num = test_controller.open_account(:savings, test_holder)
-    test_controller.open_account(:savings, second_holder)
-    num_3 = test_controller.open_account(:savings, test_holder)
+    new_id = open_account
+    open_account(:savings, second_holder)
+    new_id_3 = open_account
     expect(test_controller.get_accounts_of(test_holder.id))
-      .to eq([test_controller.accounts[num], test_controller.accounts[num_3]])
+      .to eq([test_controller.accounts[new_id], test_controller.accounts[new_id_3]])
   end
 end
