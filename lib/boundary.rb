649be4424682c1430ef46e5d25ca5a8f3d09c292
Phil Brockwell <nineteen40eight@gmail.com>
2015-07-04 23:30:09 +0100

    refactored boundary, added verify method

--- lib/boundary.rb
+++ lib/boundary.rb
@@ -1,55 +1,6 @@
 require './lib/controller'
 # Definition of Boundary Class
 class Boundary
-  def initialize
-    @controller = Controller.new
-  end
-
-  def show_menu
-    MENU_ITEMS.each { |key, value| puts_with_sleep "#{key}. #{value}" }
-    puts_with_sleep 'Make a selection and Press Enter'
-  end
-
-  def start
-    show_menu
-    input = gets.chomp
-    until MENU_ITEMS.key? input
-      puts_with_sleep 'Unrecognised option, try again...'
-      show_menu
-      input = gets.chomp
-    end
-    select_option(input)
-  end
-
-  def select_option(input)
-    create_holder if input.to_i == 0
-    create_account if input.to_i == 1
-    start
-  end
-
-  def create_holder
-    puts 'Enter Name and Press Enter'
-    name = gets.chomp
-    response = @controller.create_holder name
-    puts response
-  end
-
-  def create_account
-    ACCOUNT_TYPES.each { |key, value| puts_with_sleep "#{key}. #{value} Account" }
-    puts_with_sleep 'Make a selection and Press Enter'
-    input = gets.chomp
-    until MENU_ITEMS.key? input
-      puts_with_sleep 'Unrecognised option, try again...'
-      ACCOUNT_TYPES.each { |key, value| puts_with_sleep "#{key}. #{value} Account" }
-      input = gets.chomp
-    end
-    type = ACCOUNT_TYPES[input]
-    puts_with_sleep 'Enter Holder ID'
-    holder_id = gets.chomp.to_i
-    response = @controller.open_account(type, with: holder_id)
-    puts response
-  end
-
   MENU_ITEMS = { '0' => 'Create New Holder',
                  '1' => 'Create an Account',
                  '2' => 'Make a Deposit',
@@ -68,11 +19,58 @@ class Boundary
                     '4' => :SMB,
                     '5' => :Student }
 
+  def initialize
+    @controller = Controller.new
+  end
+
+  def start
+    show(MENU_ITEMS)
+    input = verify(gets.chomp, with: MENU_ITEMS)
+    select_option(input)
+  end
+
   private
+
+  def verify(input, with:)
+    until with.key? input
+      puts_with_sleep 'Unrecognised option, try again...'
+      show(with)
+      input = gets.chomp
+    end
+    input
+  end
+
+  def create_holder
+    puts 'Enter Name and Press Enter'
+    name = gets.chomp
+    response = @controller.create_holder name
+    puts response
+  end
+
+  def create_account
+    show(ACCOUNT_TYPES)
+    input = verify(gets.chomp, with: ACCOUNT_TYPES)
+    type = ACCOUNT_TYPES[input]
+    puts_with_sleep 'Enter Holder ID'
+    holder_id = gets.chomp.to_i
+    response = @controller.open_account(type, with: holder_id)
+    puts response
+  end
 
   def puts_with_sleep(string)
     puts string
     sleep(0.5)
+  end
+
+  def show(list)
+    list.each { |key, value| puts_with_sleep "#{key}. #{value}" }
+    puts_with_sleep 'Make a selection and Press Enter'
+  end
+
+  def select_option(input)
+    create_holder if input.to_i == 0
+    create_account if input.to_i == 1
+    start
   end
 end
 
