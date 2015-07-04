6c109658d5ce0f734f495ea7561682fbfd14186c
Phil Brockwell <nineteen40eight@gmail.com>
2015-07-04 21:48:57 +0100

    menu method

--- lib/boundary.rb
+++ lib/boundary.rb
@@ -1,11 +1,36 @@
+# Definition of Boundary Class
 class Boundary
-
-  def menu
-    puts 'Enter 1 for Add Holder'
+  def show_menu
+    MENU_ITEMS.each { |key, value| puts_with_sleep "#{key}. #{value}" }
+    puts_with_sleep 'Make a selection and Press Enter'
   end
 
+  def start
+    show_menu
+    until MENU_ITEMS.has_key? gets.chomp
+      puts_with_sleep 'Unrecognised option, try again...'
+      show_menu
+    end
+  end
+
+  MENU_ITEMS = { '0' => 'Create New Holder',
+                 '1' => 'Create an Account',
+                 '2' => 'Make a Deposit',
+                 '3' => 'Display Account Balance',
+                 '4' => 'Make a Withdrawal',
+                 '5' => 'Make a Transfer',
+                 '6' => 'Pay Interest',
+                 '7' => 'Add Account ',
+                 '8' => 'Show Customers Accounts',
+                 '9' => 'View Account Transactions' }
+
+  private
+
+  def puts_with_sleep(string)
+    puts string
+    sleep(0.5)
+  end
 end
 
 test = Boundary.new
-
-test.menu
+test.start
