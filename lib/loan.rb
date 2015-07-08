656dcd9927d606e2f3d3dbc89fe18ad50fa8d25d
Phil Brockwell <nineteen40eight@gmail.com>
2015-07-08 23:28:05 +0100

    9 tests added for loan class, all passing

--- lib/loan.rb
+++ lib/loan.rb
@@ -3,7 +3,8 @@ require 'date'
 class Loan
 
   attr_accessor :amount_borrowed, :holder, :rate,
-                :term, :repayment_date, :payments
+                :term, :repayment_date, :transactions,
+                :outstanding
 
   def initialize(options={})
     @amount_borrowed = options[:borrowed]
@@ -11,6 +12,19 @@ class Loan
     @term = options[:term]
     @rate = options[:rate]
     @repayment_date = DateTime.now >> (12 * @term)
-    @payments = []
+    @transactions = []
+    @outstanding = @amount_borrowed + (amount_borrowed * rate)
+  end
+
+  def make_payment(amount)
+    @outstanding -= amount
+    new_transaction = Transaction.new(:loan_payment, amount)
+    add_transaction new_transaction
+  end
+
+  private
+
+  def add_transaction(transaction)
+    @transactions << transaction
   end
 end
--- spec/loan_spec.rb
+++ spec/loan_spec.rb
@@ -1,14 +1,15 @@
+require 'timecop'
 require 'loan'
 
 describe 'Loan' do
-  context 'when initialised' do
-    let(:test_holder) { double :holder }
+  let(:test_holder) { double :holder }
 
-    def create_loan
-      options = { holder: test_holder, borrowed: 1000, term: 2, rate: 2 }
-      loan = Loan.new(options)
-    end
-    
+  def create_loan
+    options = { holder: test_holder, borrowed: 1000, term: 2, rate: 2 }
+    loan = Loan.new(options)
+  end
+
+  context 'when initialised' do
     it 'knows how much was borrowed' do
       loan = create_loan
       expect(loan.amount_borrowed).to eq(1000)
@@ -17,6 +18,13 @@ describe 'Loan' do
     it 'has an interest rate' do
       loan = create_loan
       expect(loan.rate).to eq(2)
+    end
+
+    it 'calculates the amount outstanding' do
+      loan = create_loan
+      rate = loan.rate
+      amount = loan.amount_borrowed
+      expect(loan.outstanding).to eq(amount + (amount * rate))
     end
 
     it 'has a holder' do
@@ -35,9 +43,27 @@ describe 'Loan' do
       expect(loan.repayment_date.year - today.year).to be(2)
     end
 
-    it 'has an empty payments array' do
+    it 'has an empty transactions array' do
       loan = create_loan
-      expect(loan.payments).to eq([])
+      expect(loan.transactions).to eq([])
+    end
+  end
+
+  context 'making payments' do
+    it 'can make a payment' do
+      loan = create_loan
+      expect { loan.make_payment(100) }
+        .to change { loan.outstanding }.by (-100)
+    end
+
+    it 'adds a transaction to the array' do
+      loan = create_loan
+      Timecop.freeze
+      loan.make_payment(100)
+      expect(loan.transactions.last.type).to eq(:loan_payment)
+      expect(loan.transactions.last.amount).to eq(100)
+      expect(loan.transactions.last.date).to eq(Time.now)
+      Timecop.return
     end
   end
 end
